0ac9a75b60f35421c9dd70f8e422012c7680e506
Yamen <62349907+yamenAl@users.noreply.github.com>
2025-10-30 23:47:31 +0100

    update netlify #1
    update netlify for live site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+ import { defineConfig } from 'astro/config';
+ import netlify from '@astrojs/netlify';
 
-// https://astro.build/config
-export default defineConfig({});
+ export default defineConfig({
+    // ...
+    adapter: netlify(),
+ });
